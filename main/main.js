ff4f02e7160b3655658975468092f548e7ec7a67
Nicholas L. Goodman <nicholas.lee.goodman@gmail.com>
2016-02-24 21:48:34 -0600

    Fixed bug that would end the timebox on a pause

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 /**
  * TimeSlice by Nicholas L. Goodman
  * Allows basic "timebox" time management for increased productivity. 
- * It is provided under a GPL license.
+ * Licensed under GPL.
  * https://gnu.org/licenses/gpl-3.0.txt
  * Cuckoo Sound Effect by MorgantJ under a Creative Commons License
  * http://creativecommons.org/licenses/by/3.0/legalcode
@@ -11,18 +11,29 @@
 
 
 window.onload = function() {
+	
+	document.getElementById("timeBoxes").style.border="1px solid gray";
+	document.getElementById("breaks").style.border="1px solid gray";
+	document.getElementById("timeBoxes").style.padding="2px";
+	document.getElementById("breaks").style.padding="2px";
+	
+	// evil global variables, fix later
 	var boxes = 0;
 	var breaks = 0;
 	var completed = false;
 	var box_duration = $('#box_duration').val();
 	var count = 0;
 	var iter = 4;
+	var iter_original = 4;
 	var short_break_duration = $('#break_short').val();
 	var long_break_duration = $('#break_long').val();
 	var on_short_break = false;
 	var on_long_break = false;
 	
+	// tock *bizarrely* has no "get_state" or similar
 	var isRunning = false;
+	var isPaused = false;
+	
 	var cuckoo = new Audio('cuckoo.mp3');
 	
 	function proceed () {
@@ -39,7 +50,7 @@ window.onload = function() {
 			on_short_break = false;			
 		}
 		else if (on_long_break){
-			cuckoo.play();
+			//cuckoo.play();
 			breaks = 0;
 			boxes = 0;			
 			//$('#time_left').val($('#break_long').val());
@@ -73,15 +84,18 @@ window.onload = function() {
            }
     });
     
+    
+    // tock has no "play/pause" function
   $('#start_box').on('click', function () {
   		cuckoo.pause();
   		if (isRunning) {
   			isRunning = false;
+  			isPaused = true;
   			timebox.stop($('#time_left').val());        	
         }
         else {
         	isRunning = true; 		
-			proceed();
+			timebox.start($('#time_left').val());
         }
     });
     
@@ -95,7 +109,6 @@ window.onload = function() {
         $('#breaks').text(breaks);
         
     });
-    
-    
+      
     
 };
