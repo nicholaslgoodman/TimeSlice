3b3d33835a6719f9df83cfc76e0ab31a3dfe0fa4
Nicholas Goodman <nicholas.lee.goodman@gmail.com>
2016-02-27 21:22:07 -0600

    now fully OO; misc other changes

--- main/main.js
+++ main/main.js
@@ -9,106 +9,149 @@
  * Tock provided by Mr Chimp under the MIT License
  */
 
+// note: must import tock.js
 
 window.onload = function() {
+	/*
+	 * This is our main function.
+	 */
 	
+	// temporary style code
 	document.getElementById("timeBoxes").style.border="1px solid gray";
 	document.getElementById("breaks").style.border="1px solid gray";
 	document.getElementById("timeBoxes").style.padding="2px";
 	document.getElementById("breaks").style.padding="2px";
-	
-	// evil global variables, fix later
-	var boxes = 0;
-	var breaks = 0;
-	var completed = false;
-	var box_duration = $('#box_duration').val();
-	var count = 0;
-	var iter = 4;
-	var iter_original = 4;
-	var short_break_duration = $('#break_short').val();
-	var long_break_duration = $('#break_long').val();
-	var on_short_break = false;
-	var on_long_break = false;
-	
-	// tock *bizarrely* has no "get_state" or similar
-	var isRunning = false;
-	var isPaused = false;
-	
-	var cuckoo = new Audio('cuckoo.mp3');
-	
-	function proceed () {
-		
-		if (boxes >= iter){
-			on_long_break = true;
-			on_short_break = false;
-		}
-		
-		if (on_short_break){			
-			breaks++;
-			//$('#time_left').val($('#break_short').val());
-			timebox.start($('#break_short').val());
-			on_short_break = false;			
-		}
-		else if (on_long_break){
-			//cuckoo.play();
-			breaks = 0;
-			boxes = 0;			
-			//$('#time_left').val($('#break_long').val());
-			timebox.start($('#break_long').val());
-			on_long_break = false;
-			iter = iter_original;
-		}
-		else {
-			boxes++;
-			//$('#time_left').val($('#box_duration').val());
-			timebox.start($('#box_duration').val());
-			on_short_break = true;
-			
-		};       
-    	
-         $('#timeBoxes').text(boxes);
-         $('#breaks').text(breaks);
-	   };
 	   
-    var timebox = Tock({
-    	countdown: true,
-        interval: 250,
-        callback: function () {
-            console.log(timebox.lap() / 1000);
-            $('#time_left').val(timebox.msToTime(timebox.lap()));
-        },
-        complete: function () {
-            console.log('end');
-            cuckoo.play();         
-			proceed();            
-           }
-    });
-    
-    
-    // tock has no "play/pause" function
-  $('#start_box').on('click', function () {
-  		cuckoo.pause();
-  		if (isRunning) {
-  			isRunning = false;
-  			isPaused = true;
-  			timebox.stop($('#time_left').val());        	
+    function TimeSlice (audioFile, options) {
+    /*
+     * Time Slice is a child of Tock and adds some new functions
+     * @param audioFile should be a valid string to the path of a valid audio file
+     * @param options is passed to Tock 
+     */
+    	
+	Tock.call(this, options);
+	
+	this.timeLeft = 25;
+	this.sliceCount = 0;
+	this.sliceDuration = $('#box_duration').val();
+	this.shortBreakDuration = $('#break_short').val();
+	this.shortBreakCount = 0;
+	this.longBreakDuration = $('#break_long').val();
+	this.running = false;
+	this.iterations = 4;
+	this.iterationsOriginal = 4;
+	this.onShortBreak = false;
+	this.onLongBreak = false;
+	this.alarmAudio = new Audio(audioFile);
+	
+};
+
+TimeSlice.prototype = Object.create(Tock.prototype);
+
+TimeSlice.prototype.constructor = TimeSlice;
+
+TimeSlice.prototype.isRunning = function(){
+	/*
+	 * Tock does not have a similar function, and we need it
+	 */
+	return this.running;
+};
+
+TimeSlice.prototype.playPause = function(){
+	/*
+	 * this combines the start/stop/pause functions of Tock into one; simple
+	 */
+	
+  		this.pauseAlarm();
+  		
+  		if (this.running) {
+  			this.running = false;  			
+  			MySlice.stop($('#time_left').val());        	
         }
         else {
-        	isRunning = true; 		
-			timebox.start($('#time_left').val());
+        	this.running = true; 		
+			MySlice.start($('#time_left').val());
         }
+};
+ 
+TimeSlice.prototype.reset = function(){
+	/*
+	 * Replaces Tock's function with behavior more appropriate to slice
+	 */
+	    this.running = false;
+        MySlice.stop();
+        $('#time_left').val($('#box_duration').val());
+        this.sliceCount = 0;
+        this.breakCount = 0;
+        $('#timeBoxes').text(this.sliceCount);
+        $('#breaks').text(this.breakCount);
+ };  
+     
+TimeSlice.prototype.playAlarm = function(){
+	this.alarmAudio.play();
+	};
+
+TimeSlice.prototype.pauseAlarm = function(){
+	this.alarmAudio.pause();
+	};
+
+
+TimeSlice.prototype.proceed = function(){
+	/*
+	 * Most of the logic is stored here for now
+	 */
+	
+	if (this.sliceCount >= this.iterations){
+			this.onLongBreak = true;
+			this.onShortBreak = false;
+		}
+		
+		if (this.onShortBreak){			
+			this.breakCount++;
+			MySlice.start($('#break_short').val());
+			this.onShortBreak = false;			
+		}
+		
+		else if (this.onLongBreak){			
+			this.breakCount = 0;
+			this.sliceCount = 0;			
+			MySlice.start($('#break_long').val());
+			this.onLongBreak = false;
+			this.iterations = this.iterationsOriginal;
+		}
+		else {
+			this.sliceCount++;
+			MySlice.start($('#box_duration').val());
+			this.onShortBreak = true;			
+		};       
+    	
+         $('#timeBoxes').text(this.sliceCount);
+         $('#breaks').text(this.breakCount);
+	   };
+   
+var AUDIO_FILE = "media/cuckoo.mp3";
+
+var MySlice = new TimeSlice(AUDIO_FILE, {
+    countdown: true,
+    interval: 250,
+    callback: function () {    	
+    	$('#time_left').val(MySlice.msToTime(MySlice.lap()));
+    },
+    complete: function () {
+        console.log('end');
+        MySlice.playAlarm();         
+		MySlice.proceed();            
+    }
+});
+
+	// temporary method of handling buttons.
+  $('#start_box').on('click', function () {
+  		MySlice.playPause();
     });
     
     $('#reset_box').on('click', function () {
-    	isRunning = false;
-        timebox.stop();
-        $('#time_left').val($('#box_duration').val());
-        boxes = 0;
-        breaks = 0;
-        $('#timeBoxes').text(boxes);
-        $('#breaks').text(breaks);
-        
+		MySlice.reset();       
     });
       
-    
+   
 };
